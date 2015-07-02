5eff00080547aaa404f088930d90e3494acf575f
RHays91 <rhaysvm42@gmail.com>
2015-07-02 10:45:52 -0700

    solved linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if (list.head === null && list.tail === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      var curTail = list.tail;
+      list.tail = node;
+      curTail.next = node;
+    }
   };
 
   list.removeHead = function(){
+    var prevHead = list.head;
+    delete list.head;
+    list.head = prevHead.next
+    return prevHead.value;
+
   };
 
   list.contains = function(target){
+    var temp = list.head;
+    while (temp.value !== target) {
+      temp = temp.next;
+      if (temp === null) {
+        return false;
+      }
+    }
+    return true;
   };
 
   return list;
