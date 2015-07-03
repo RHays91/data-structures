0f28bfc59bcf0f376d0720bd1f4e8e15530ad60f
RHays91 <rhaysvm42@gmail.com>
2015-07-02 21:06:14 -0700

    Graph: First 3 tests passing

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,21 +1,36 @@
 
 
 var Graph = function(){
+
+	this.storage = [];
 };
 
 Graph.prototype.addNode = function(node){
+	this.storage.push(node);
 };
 
 Graph.prototype.contains = function(node){
+	for(var i = 0; i < this.storage.length; i++){
+		if(this.storage[i] === node){
+			return true;
+		}
+	}
+	return false;
 };
 
 Graph.prototype.removeNode = function(node){
+	for(var i = 0; i < this.storage.length; i++){
+		if(this.storage[i] === node){
+			this.storage.splice(i, 1);
+		}
+	}
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
