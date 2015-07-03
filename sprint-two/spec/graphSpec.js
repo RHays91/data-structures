28ddfa2e1b981def5a49ea7c0acc87da620495c8
RHays91 <rhaysvm42@gmail.com>
2015-07-03 11:22:56 -0700

    added test to confirm graph is undirected, using object to store edges

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -36,11 +36,21 @@ describe('graph', function() {
     expect(graph.hasEdge('penguins', 'kittens')).to.equal(false);
   });
 
+  it('should create symmetrical relationships when creating edges', function() {
+    graph.addNode('puppies');
+    graph.addNode('kittens');
+    graph.addNode('penguins');
+    graph.addEdge('penguins', 'puppies');
+    expect(graph.hasEdge('penguins', 'puppies')).to.equal(true);
+    expect(graph.hasEdge('puppies', 'penguins')).to.equal(true);
+  });
+
   it('should remove edges between nodes', function() {
     graph.addNode('apples');
     graph.addNode('satsumas');
     graph.addEdge('satsumas', 'apples');
-    expect(graph.hasEdge('apples', 'satsumas')).to.equal(true);
+    graph.removeEdge('satsumas', 'apples');
+    expect(graph.hasEdge('apples', 'satsumas')).to.equal(false);
   });
 
   it('should execute a callback on each node in the graph', function() {
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,16 +2,17 @@
 
 var Graph = function(){
 
-	this.storage = [];
+	this.nodes = [];
+	this.edges = {};
 };
 
 Graph.prototype.addNode = function(node){
-	this.storage.push(node);
+	this.nodes.push(node);
 };
 
 Graph.prototype.contains = function(node){
-	for(var i = 0; i < this.storage.length; i++){
-		if(this.storage[i] === node){
+	for(var i = 0; i < this.nodes.length; i++){
+		if(this.nodes[i] === node){
 			return true;
 		}
 	}
@@ -19,24 +20,50 @@ Graph.prototype.contains = function(node){
 };
 
 Graph.prototype.removeNode = function(node){
-	for(var i = 0; i < this.storage.length; i++){
-		if(this.storage[i] === node){
-			this.storage.splice(i, 1);
+	for(var i = 0; i < this.nodes.length; i++){
+		if(this.nodes[i] === node){
+			this.nodes.splice(i, 1);
 		}
 	}
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	// for(var i = 0; i < this.edges.length; i++){
+	// 	if(this.edges[i][0] === fromNode && this.edges[i][1] === toNode){
+	// 		return true;
+	// 	}
+	// }
+	// return false;
+
+	for(var i = 0; i < this.edges[fromNode].length; i++){
+		if(this.edges[fromNode][i] === toNode){
+			return true;
+		}
+	}
+	for(var i = 0; i < this.edges[toNode].length; i++){
+		if(this.edges[toNode][i] === fromNode){
+			return true;
+		}
+	}	
+	return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-	
+	if(this.edges[fromNode] === undefined){
+		this.edges[fromNode] = [toNode];
+	} else {
+		this.edges[fromNode].push(toNode);
+	}
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	this.edges[fromNode].splice
 };
 
-Graph.prototype.forEachNode = function(cb){
+Graph.prototype.forEachNode = function(callback){
+	for(var i = 0; i < this.nodes.length; i++){
+		callback(this.nodes[i]);
+	}
 };
 
 /*
