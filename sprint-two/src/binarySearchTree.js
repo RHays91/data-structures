6dd7638b93757a64ebd03b04ed2ce999f40e6c61
RHays91 <rhaysvm42@gmail.com>
2015-07-03 16:31:38 -0700

    Binary Search Tree passing 1st Test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,16 @@
 var BinarySearchTree = function(value){
+	var Tree = Object.create(methods);
+
+	return Tree;
 };
 
+var methods = {};
 
+methods.insert = function(value) {};
+
+methods.contains = function(value) {};
+
+methods.depthFirstLog = function(func) {};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
