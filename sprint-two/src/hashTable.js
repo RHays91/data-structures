55cce6fd51b0d5711ee021c69e32c3781fc523bf
RHays91 <rhaysvm42@gmail.com>
2015-07-03 16:31:18 -0700

    Completed hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,38 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = {};
+  bucket[k] = v;
+
+  if(this._storage.get(i) === undefined){
+  	this._storage.set(i, bucket);
+  } else {
+  	var temp = this._storage.get(i);
+  	temp[k] = v;
+  	this._storage.set(i, temp);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+
+  if (bucket[k] !== undefined) {
+  	return bucket[k];
+  } else {
+  	
+  } 
 
 };
 
 HashTable.prototype.remove = function(k){
+	var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var bucket = this._storage.get(i);
+  if(bucket[k] !== undefined){
+    bucket[k] = null;
+  }
 };
 
 
