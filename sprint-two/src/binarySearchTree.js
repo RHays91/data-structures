4f8f8d0d49b2f6244e92b76aaa3d8ee7b2a9268e
RHays91 <rhaysvm42@gmail.com>
2015-07-03 21:46:24 -0700

    Working on second test binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,12 +1,30 @@
 var BinarySearchTree = function(value){
 	var Tree = Object.create(methods);
+	Tree.left = null;
+	Tree.right = null;
+	Tree.value = value;
 
 	return Tree;
 };
 
 var methods = {};
 
-methods.insert = function(value) {};
+methods.insert = function(value) {
+	var node = BinarySearchTree(value);
+
+	this.left.insert(value)
+
+	if(value < this.value){
+		//then the node should be inserted on the left, if the left is null
+		if(this.left === null){
+			this.left = node;
+		} else {
+			this.left.insert(value)
+		}
+	} else if(value > this.value){
+		this.right = node;
+	}
+};
 
 methods.contains = function(value) {};
 
